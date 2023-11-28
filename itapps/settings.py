401d7e30d6e4bb6bef2c6e4f1c4b7d94826af76c
C1026327 <c1026327@my.shu.ac.uk>
2023-11-28 17:41:17 +0000

    send emails woo

--- itapps/settings.py
+++ itapps/settings.py
@@ -156,5 +156,9 @@ REST_FRAMEWORK = {
     ],
 }
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = BASE_DIR / 'emails'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'notallowedsmtp@gmail.com'
+EMAIL_HOST_PASSWORD = 'yaji ustk weuy blvo'
